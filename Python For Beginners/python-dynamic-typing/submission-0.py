d80f2ddc8007ea74e55afeada172315902a362a1
Akhil Kondapaneni <58919890+Akhil120@users.noreply.github.com>
2026-07-21 23:38:26 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10 
+print(type(variable))
+variable = 10.00
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Hello"    # str type
+print(type(variable))
+variable = [1, 2, 3]  # list type
+print(type(variable))
